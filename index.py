1eba8a397dbb027d46e222fdfe3b76a295f48cb4
Millionpkp <73513663+SirDevi@users.noreply.github.com>
2022-04-19 18:46:36 -0500

    a

---
+++ index.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    return render_template('index.html')
+
+@app.route('/contacto')
+def contato():
+    return render_template('contacto.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
